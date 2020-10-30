b8f035a0c81bc8ae1278bdb22c2ca4257d3265a3
Giovanni Catania <giovanni.catania@polito.it>
2020-10-30 03:22:10 +0100

    h0 as keyword arg

--- src/types.jl
+++ src/types.jl
@@ -34,7 +34,7 @@ struct DCState
     Sc  :: Vector{Matrix}			# array of cavity inverse correlation matrix (one for each factor)
 end
 
-function DCState(N::Int,m::Vector{Int},h0::Float64;f::Function=n->h0*(rand(n) .- 0.5))
+function DCState(N::Int,m::Vector{Int};h0::Float64 = 1e-3,f::Function=n->h0*(rand(n) .- 0.5))
     M=length(m)
     return DCState([zeros(m[a]) for a=1:M],
                    [zeros(m[a],m[a]) for a=1:M],
